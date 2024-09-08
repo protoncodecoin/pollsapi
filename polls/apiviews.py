c8341a6b7fc8c872536e9b997c76e283a696c748
Prince Asante <princeaffumasante@gmail.com>
2024-09-08 00:08:30 +0000

    initial commit

---
+++ polls/apiviews.py
@@ -0,0 +1,46 @@
+from rest_framework.views import APIView
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.shortcuts import get_object_or_404
+
+from .models import Poll, Choice
+from .serializers import ChoiceSerializer, PollSerializer, VoteSerializer
+
+
+# class PollList(generics.ListCreateAPIView):
+#     queryset = Poll.objects.all()
+#     serializer_class = PollSerializer
+
+
+# class PollDetail(generics.RetrieveDestroyAPIView):
+#     queryset = Poll.objects.all()
+#     serializer_class = PollSerializer
+
+
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
+        return queryset
+
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(APIView):
+    serializer_class = VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {"choice": choice_pk, "poll": pk, "voted_by": voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
